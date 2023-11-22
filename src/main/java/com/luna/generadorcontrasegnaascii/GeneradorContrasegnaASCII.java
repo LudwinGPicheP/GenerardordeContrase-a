c91d84bb4a825663d45614097b7a5a20f8fb5018
Ludwing Piche <ludgianpichepaz@gmail.com>
2023-11-21 19:54:09 -0600

    subiendo proyecto a git

---
+++ src/main/java/com/luna/generadorcontrasegnaascii/GeneradorContrasegnaASCII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.luna.generadorcontrasegnaascii;
+
+/**
+ *
+ * @author luna
+ */
+public class GeneradorContrasegnaASCII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
